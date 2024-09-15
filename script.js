7fd7ed656ac1274f86438e7c93bae795371e897c
drawdlu <152858099+drawdlu@users.noreply.github.com>
2024-09-15 22:07:05 +0800

    Create initial card divs

---
+++ script.js
@@ -0,0 +1,23 @@
+const INITIAL_CARD_COUNT = 12;
+const ROW_NUMBER = 3;
+
+function createCards(cardCount) {
+    const container = document.querySelector('.container');
+    for (let i = 0; i < ROW_NUMBER; i++) {
+        const row = document.createElement('div')
+        row.className = 'row';
+        const columnCount = cardCount / ROW_NUMBER;
+        createCardColumn(row, columnCount);
+        container.appendChild(row);
+    }
+}
+
+function createCardColumn(rowDiv, columnCount) {
+    for (let i = 0; i < columnCount; i++) {
+        const card = document.createElement('div');
+        card.className = 'card';
+        rowDiv.appendChild(card);
+    }
+}
+
+createCards(INITIAL_CARD_COUNT);
