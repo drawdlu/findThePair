860a4cd0c95303264c66c48681e72507b6d0c675
drawdlu <152858099+drawdlu@users.noreply.github.com>
2024-09-16 19:24:16 +0800

    Add game timer bar

--- script.js
+++ script.js
@@ -5,9 +5,12 @@ const TRANSITION_TIME = 1000;
 const GAME_TIMER = 60000;
 const SHOW_CARD = 'show';
 const HIDE_CARD = 'hide';
+const BAR_WIDTH = 90;
+const MS_PER_SECOND = 1000;
 
 const startBtn = document.querySelector('#start');
 
+let stillPlaying = true;
 let numOfCards = INITIAL_CARD_COUNT;
 let cardOne = null; 
 let cardTwo = null;
@@ -169,15 +172,34 @@ function checkScore() {
 
 function gameTimer() {
     setTimeout(endGame , GAME_TIMER);
+    const remPercent = getPercentToSubtract();
+    setInterval( () => {
+        updateGameBar(remPercent)
+    }, MS_PER_SECOND)
+}
+
+function getPercentToSubtract() {
+    return (INITIAL_WIDTH.slice(0, -2) / (GAME_TIMER / MS_PER_SECOND)).toFixed(1)
 }
 
 function endGame() {
+    stillPlaying = false;
     if (score < numOfCards / 2) {
         toggleAllCards(SHOW_CARD);
         alert(`You didn't find all the cards`)
     } else {
         alert('You won!')
     }
+}
+
+const bar = document.querySelector('.timeBar');
+const INITIAL_WIDTH = window.getComputedStyle(bar).width;
+
+let width = INITIAL_WIDTH;
+
+function updateGameBar(numToRemove) {
+    width = ((width.slice(0, -2)) - numToRemove).toFixed(1) + 'px';
+    bar.style.width = width;
 }
 
 createCards(numOfCards);
